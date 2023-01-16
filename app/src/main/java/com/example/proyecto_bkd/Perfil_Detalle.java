d9989e486a3565e91ba6b93a82c6d3ff7515a42a
Miguel Ángel desde clase <miguelsegoviafreeman@gmail.com>
2023-01-16 20:11:48 +0100

    Creado perfiles.

---
+++ app/src/main/java/com/example/proyecto_bkd/Perfil_Detalle.java
@@ -0,0 +1,14 @@
+package com.example.proyecto_bkd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Perfil_Detalle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_detalle);
+    }
+}
