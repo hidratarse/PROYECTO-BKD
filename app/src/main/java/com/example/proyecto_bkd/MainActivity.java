b30a8cbb89129e4029a5336b3f1532419ccd1680
JohnHawkinsGit <jorge.mhuguet@gmail.com>
2023-01-16 20:06:09 +0100

    actividad login y partida

--- app/src/main/java/com/example/proyecto_bkd/MainActivity.java
+++ app/src/main/java/com/example/proyecto_bkd/MainActivity.java
@@ -1,19 +1,38 @@
 package com.example.proyecto_bkd;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "";
-
+    Button bLogin;
+    static String texto=".";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Log.d(TAG, "LOL");
+        bLogin=findViewById(R.id.bLogin);
+
+        bLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Partida.class);
+                intent.putExtra(texto, "Hola");
+                startActivity(intent);
+            }
+        });
+
     }
 }
