2b98c478e8f2fd6fce2d2e60c495e4a7578661de
Miguel Ángel desde clase <miguelsegoviafreeman@gmail.com>
2023-01-20 19:47:16 +0100

    Terminado: - activity_perfil_detalle.xml Añadidos: -activity_perfiles -Actualizados algunos strings

---
+++ app/src/main/java/com/example/proyecto_bkd/activity_perfiles.java
@@ -0,0 +1,14 @@
+package com.example.proyecto_bkd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_perfiles extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfiles);
+    }
+}
