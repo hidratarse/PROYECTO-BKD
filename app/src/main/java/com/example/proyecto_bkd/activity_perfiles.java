9c2c1f0913f75ad2ceb80e1530d8f7b7e5427f4f
Miguel Ángel desde clase <miguelsegoviafreeman@gmail.com>
2023-01-20 20:06:45 +0100

    Planteado el recyclerview.

--- app/src/main/java/com/example/proyecto_bkd/activity_perfiles.java
+++ app/src/main/java/com/example/proyecto_bkd/activity_perfiles.java
@@ -1,14 +1,25 @@
 package com.example.proyecto_bkd;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class activity_perfiles extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfiles);
+
+        recyclerView = findViewById(R.id.recycled_perfiles);
+
+        recyclerView.setHasFixedSize(true);
+
+        recyclerView.setLayoutManager(new GridLayoutManager());
+
     }
 }
