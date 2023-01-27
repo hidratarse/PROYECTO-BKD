0296ef1cb30a44cb0b64cd2fed77459b56f4add2
Miguel Ángel desde clase <miguelsegoviafreeman@gmail.com>
2023-01-27 18:39:43 +0100

    Añadido ranking, falta por completarlo.

---
+++ app/src/main/java/com/example/proyecto_bkd/perfiles/Perfil_Detalle.java
@@ -0,0 +1,16 @@
+package com.example.proyecto_bkd.perfiles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.proyecto_bkd.R;
+
+public class Perfil_Detalle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_detalle);
+    }
+}
