b8c69243a67d3092ed5353549a469b56b6df8e29
JohnHawkinsGit <jorge.mhuguet@gmail.com>
2023-01-30 21:39:08 +0100

    modificado layout recursos

--- app/src/main/java/com/example/proyecto_bkd/partida/ResumenTurno.java
+++ app/src/main/java/com/example/proyecto_bkd/partida/ResumenTurno.java
@@ -2,7 +2,12 @@ package com.example.proyecto_bkd.partida;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.ImageView;
+
 import com.example.proyecto_bkd.R;
 
 public class ResumenTurno extends AppCompatActivity {
@@ -10,6 +15,5 @@ public class ResumenTurno extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_resumen_turno);
     }
 }
