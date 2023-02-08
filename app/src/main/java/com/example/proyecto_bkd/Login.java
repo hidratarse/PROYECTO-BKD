1ed2b4ddfbe2a3e832cf7ccdd7fc795cbc16f621
JohnHawkinsGit <jorge.mhuguet@gmail.com>
2023-02-08 13:54:11 +0100

    musica, icono, nombre y alguna cosa mas

--- app/src/main/java/com/example/proyecto_bkd/Login.java
+++ app/src/main/java/com/example/proyecto_bkd/Login.java
@@ -3,6 +3,7 @@ package com.example.proyecto_bkd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -11,17 +12,22 @@ import android.widget.Button;
 import com.example.proyecto_bkd.partida.Partida;
 
 public class Login extends AppCompatActivity {
-
+    MediaPlayer mp, puerta;
     Button bLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mp=MediaPlayer.create(this, R.raw.alexandernakaradagatesofglory);
+        puerta=MediaPlayer.create(this,R.raw.puerta);
         bLogin=findViewById(R.id.bLogin);
+        mp.start();
+
         bLogin.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
+                puerta.start();
                 Intent intent = new Intent(Login.this, Partida.class);
                 startActivity(intent);
                 finish();
