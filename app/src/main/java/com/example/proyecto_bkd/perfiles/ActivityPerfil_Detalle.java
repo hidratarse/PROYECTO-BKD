d0d79b2f4f770d4e2385e4a5a873a3b42651dc56
JohnHawkinsGit <jorge.mhuguet@gmail.com>
2023-02-10 19:39:06 +0100

    al 100% de verdad

--- app/src/main/java/com/example/proyecto_bkd/perfiles/ActivityPerfil_Detalle.java
+++ app/src/main/java/com/example/proyecto_bkd/perfiles/ActivityPerfil_Detalle.java
@@ -4,20 +4,75 @@ import static android.content.ContentValues.TAG;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.proyecto_bkd.R;
+import com.example.proyecto_bkd.partida.Partida;
+import com.example.proyecto_bkd.ranking.activity_ranking2;
 
 public class ActivityPerfil_Detalle extends AppCompatActivity {
     TextView nombre;
+    ImageButton bImgPartidas,bImgPerfiles,bImgRanking;
+    TextView tCancela, tModificar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_detalle);
-
+        bImgPartidas=findViewById(R.id.bImgPartidas);
+        bImgPerfiles=findViewById(R.id.bImgPerfiles);
+        bImgRanking=findViewById(R.id.bImgRanking);
         nombre = findViewById(R.id.tNom);
+        tCancela=findViewById(R.id.tCancela);
+        tModificar=findViewById(R.id.tModificar);
+
+        tModificar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ActivityPerfil_Detalle.this, activity_perfiles.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        tCancela.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ActivityPerfil_Detalle.this, activity_perfiles.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        bImgRanking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ActivityPerfil_Detalle.this, activity_ranking2.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        bImgPartidas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ActivityPerfil_Detalle.this, Partida.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        bImgPerfiles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ActivityPerfil_Detalle.this, activity_perfiles.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
         Perfil perfil = (Perfil) getIntent().getSerializableExtra("PERFIL");
 
