34cf8232996ce101f9259d32f6257e5fefec59bd
Miguel A. Freeman <miguelsegoviafreeman@gmail.com>
2023-03-15 19:35:16 +0100

    Base de datos hecha.

--- app/src/main/java/com/example/proyecto_bkd/Login.java
+++ app/src/main/java/com/example/proyecto_bkd/Login.java
@@ -5,20 +5,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
 import com.example.proyecto_bkd.partida.Partida;
 import com.example.proyecto_bkd.registrarusuario.RegistroMain;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Login extends AppCompatActivity {
     MediaPlayer mp, puerta;
     Button bLogin,bRegistrarse;
+
+    FirebaseFirestore fs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        fs = FirebaseFirestore.getInstance();
+
+        Map<String, Object> users = new HashMap<>();
+        users.put("prueba", "prueba");
+
+        fs.collection("users").add(users).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Log.d("KEK","funciona");
+            }
+        });
+
 
         mp=MediaPlayer.create(this, R.raw.alexandernakaradagatesofglory);
         puerta=MediaPlayer.create(this,R.raw.puerta);
