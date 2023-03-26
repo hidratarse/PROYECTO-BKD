721bded13e234141b0eb57a2a206a6f3e8eeef9e
JohnHawkinsGit <jorge.mhuguet@gmail.com>
2023-03-26 17:56:55 +0200

    añadidas cosas de interfaz y boton musica

---
+++ app/src/main/java/com/example/proyecto_bkd/resumenturno/SeleccionPerfiles.java
@@ -0,0 +1,16 @@
+package com.example.proyecto_bkd.resumenturno;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.proyecto_bkd.R;
+
+public class SeleccionPerfiles extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seleccion_perfiles);
+    }
+}
